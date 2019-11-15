526190a3cad31f07ca3d2d0d02774c6a3299c34e
xuepuhao <877783692@qq.com>
2019-11-15 17:34:33 +0800

    配置zuul

---
+++ i9-xiaofang-zuul/src/main/java/org/i9/xiaofang/I9XiaofangZuulApplication.java
@@ -0,0 +1,13 @@
+package org.i9.xiaofang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class I9XiaofangZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(I9XiaofangZuulApplication.class, args);
+	}
+
+}
---
+++ i9-xiaofang-zuul/src/test/java/org/i9/xiaofang/I9XiaofangZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package org.i9.xiaofang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class I9XiaofangZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
